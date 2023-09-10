cf5b2361d18c0a7da0df6da2f6b4d5c15b333efc
Manu199 <manu199gh@gmail.com>
2023-09-10 16:02:28 +0200

    [add] ticket generator css

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* 
+Chiedere il numero di chiolometri da percorre
+chiedere l'età del passeggero sulla base della quale calcolare il prezzo
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+*/
+
+// const distance = parseInt(prompt('quanti km devi percorre?'));
+
